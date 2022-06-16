f294ca224ca635ea4b86387952a4ced815e25047
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-06-17 01:40:52 +0200

    #Adding Two-Way Binding

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import ExpenseDate from '../Expenses/ExpenseDate';
 
 import './ExpenseForm.css';
 
@@ -12,9 +11,9 @@ const ExpenseForm = () => {
   const [enteredDate, setEnteredDate] = useState('');
   // Combine states approach
   // const [userInput, setUserInput] = useState({
-  //   setEnteredTitle: '',
-  //   setEnteredAmount: '',
-  //   setEnteredDate: '',
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
   // });
 
   const titleChangeHandler = (event) => {
@@ -22,7 +21,7 @@ const ExpenseForm = () => {
     // setUserInput({
     //   ...userInput,
     //   enteredTitle: event.target.value,
-    // // });
+    // });
     // setUserInput((prevState) => {
     //   return { ...prevState, enteredTitle: event.target.value };
     // }); // If your state update depends on the previous state, use this function form.
@@ -54,6 +53,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -61,7 +63,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -69,6 +75,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -78,6 +85,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-07-02"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
