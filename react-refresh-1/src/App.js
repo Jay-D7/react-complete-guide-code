03b22062b25e4c1cb0a1895265082db553429e5b
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-07-01 14:42:45 +0200

    #Writing More Complex JSX Code

--- react-refresh-1/src/App.js
+++ react-refresh-1/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>Building a First Custom Component
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 };
