940d498442c0aee1e8fa6da2e88340bf2a7fe546
Jakub Dragowski <kuba.dragowski@gmail.com>
2022-07-01 11:30:59 +0200

    Init project and and first lines of code

---
+++ react-refresh-1/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visible</p>
+    </div>
+  );
+}
+
+export default App;
+
+// This is how you'd it in Regular JS imperative:
+// const para = document.createElement('p');
+// para.textContent = 'This is also visible';
+// document.getElementById('root').append(para);
